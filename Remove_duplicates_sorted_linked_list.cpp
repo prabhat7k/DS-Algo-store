d6c84eb149c5cbd9bce72fd9988764d01a3943fe
Swati Sinha <114896313+swati335@users.noreply.github.com>
2025-10-22 18:39:51 +0530

    Add removeDuplicates function for sorted linked list
    Implement a function to remove duplicates from a sorted linked list.

---
+++ Remove_duplicates_sorted_linked_list.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* buildList(vector<int> arr) {
+    if (arr.empty()) return NULL;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < arr.size(); i++) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* curr = head;
+    while (curr && curr->next) {
+        if (curr->val == curr->next->val) {
+            curr->next = curr->next->next;
+        } else {
+            curr = curr->next;
+        }
+    }
+    return head;
+}
+
+int main() {
+    ListNode* head = buildList({1, 1, 2, 3, 3, 4});
+    cout << "Original: "; printList(head);
+    head = deleteDuplicates(head);
+    cout << "After removing duplicates: "; printList(head);
+    return 0;
+}
