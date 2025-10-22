db87e6e70ac6296bd50ca7597953a382dc3df9f1
Swati Sinha <114896313+swati335@users.noreply.github.com>
2025-10-22 18:30:31 +0530

    Implement function to find middle of linked list

---
+++ Middle_of_Linked_list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* buildList(vector<int> arr) {
+    if (arr.empty()) return NULL;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < arr.size(); i++) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+ListNode* findMiddle(ListNode* head) {
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    ListNode* head = buildList(arr);
+    ListNode* mid = findMiddle(head);
+    cout << "Middle Node: " << mid->val << endl;
+    return 0;
+}
