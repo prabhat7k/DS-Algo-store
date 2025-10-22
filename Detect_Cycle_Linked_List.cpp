cf63dbead42b60393b7e469b9f9d4e8e3a6f3d03
Swati Sinha <114896313+swati335@users.noreply.github.com>
2025-10-22 18:26:10 +0530

    Detect Cycle in a Linked List

---
+++ Detect_Cycle_Linked_List.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
+
+int main()
+{
+    // Create a list 1->2->3->4->5 and a cycle at node 3
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    // Creating cycle: node 5 points to node 3
+    head->next->next->next->next->next = head->next->next;
+
+    cout << (hasCycle(head) ? "Cycle Found" : "No Cycle") << endl;
+    return 0;
+}
