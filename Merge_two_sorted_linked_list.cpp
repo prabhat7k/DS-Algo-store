404de9729e78b159b43010904573e6959aaf38b2
Swati Sinha <114896313+swati335@users.noreply.github.com>
2025-10-22 18:31:24 +0530

    Implement merging of two sorted linked lists

---
+++ Merge_two_sorted_linked_list.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* buildList(vector<int> arr) {
+    if (arr.empty()) return NULL;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < arr.size(); i++) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
+}
+
+int main() {
+    ListNode* l1 = buildList({1, 3, 5});
+    ListNode* l2 = buildList({2, 4, 6});
+    cout << "List 1: "; printList(l1);
+    cout << "List 2: "; printList(l2);
+    ListNode* merged = mergeTwoLists(l1, l2);
+    cout << "Merged: "; printList(merged);
+    return 0;
+}
